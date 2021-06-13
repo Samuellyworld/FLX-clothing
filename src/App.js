4d05582e7edc4e0e368b0f8f4b4bf026f60ca0f1
samuel <samuellyworld@gmail.com>
2021-06-13 14:06:16 -0700

    fixing lines indentation of app.js

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ import ShopPage from './Pages/Shop/ShopPage';
 import SignInAndSignUpPage from './Pages/Sign-in-and-sign-up/Sign-in-and-sign-up';
 
 class App extends React.Component {
-
 unsubscribeFromAuth = null;
-
 componentDidMount() {
    const {setCurrentUser} = this.props
   this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
@@ -55,7 +53,4 @@ const mapDispatchToProps = dispatch => ({
  setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(
-           null, 
-       mapDispatchToProps)
-            (App);
+export default connect( null, mapDispatchToProps)(App);
