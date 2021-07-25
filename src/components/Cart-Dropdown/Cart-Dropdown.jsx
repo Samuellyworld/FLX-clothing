226e24522af280c536bac1bb5c96e7e64be057f5
samuel <samuellyworld@gmail.com>
2021-07-24 20:13:36 -0700

    fixed scroll bar and added cart item to cart dropdown

--- src/components/Cart-Dropdown/Cart-Dropdown.jsx
+++ src/components/Cart-Dropdown/Cart-Dropdown.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import CartItem from '../Cart-Item/Cart-Item';
 import CustomButton from '../Custom-Button/Custom-Button';
 import './Cart-Dropdown.scss';
 
-const cartDropdown =() => (
+const CartDropdown =({cartItems}) => (
 	 <div className='cart-dropdown'>
-	  <div className='cart-items'/>
+	  <div className='cart-items'>
+	  {
+	  	cartItems.map(cartItem => 
+	  		<CartItem key={cartItem.id} item={cartItem} />
+	  		)
+	  }
+	  </div>
+
 	  <CustomButton> GO TO CHECKOUTS </CustomButton>
 	 </div>
 		);
 
-export default cartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+   cartItems
+	})
+export default connect(mapStateToProps)(CartDropdown)
+;
---
+++ src/components/Cart-Item/Cart-Item.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Cart-Item.scss';
+
+const CartItem = ({item : {imageUrl, price, name, quantity}}) => (
+	<div className='cart-item'>
+		  <img src={imageUrl} alt='item' />
+		  <div className="item-details"> 
+			  <span className='name'>{name} </span>
+			  <span className="price">{quantity} x ${price}</span> 
+		  </div>
+	 </div>
+	);
+
+export default CartItem;
