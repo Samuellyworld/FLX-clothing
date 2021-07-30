c303e58d3870f41adc4351b06e3cf9d497606a3e
samuel <samuellyworld@gmail.com>
2021-07-30 06:59:11 -0700

    added a cart empty string when cart item in the dropdown is < 0

--- src/components/Cart-Dropdown/Cart-Dropdown.jsx
+++ src/components/Cart-Dropdown/Cart-Dropdown.jsx
@@ -11,10 +11,13 @@ const CartDropdown =({cartItems}) => (
 	 <div className='cart-dropdown'>
 	  <div className='cart-items'>
 	  {
-	  	cartItems.map(cartItem => 
-	  		<CartItem key={cartItem.id} item={cartItem} />
-	  		)
-	  }
+	  	 cartItems.length ? 
+		   (cartItems.map(cartItem => 
+		  		<CartItem key={cartItem.id} item={cartItem} />
+		  		) ) :
+		  ( <span className="empty-message"> Your cart is empty
+          </span>	)
+		}
 	  </div>
 
 	  <CustomButton> GO TO CHECKOUTS </CustomButton>
