396e4f2458e5b4d827b94306bccb7fc52dc32f7b
samuel <samuellyworld@gmail.com>
2021-09-22 15:17:39 -0700

    added local storage and session storage as redux persist to app to store cart items

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import {Provider} from 'react-redux';
-import store from './redux/store'
+import {store,persistor} from './redux/store';
+
+import  {persistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
 	  <BrowserRouter>
+	   <persistGate persistor={persistor}>
 	    <App />
+	   </persistGate>
 	  </BrowserRouter>
    </Provider>,
   document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import {persistStore} from 'redux-persist';
+
 
 const middlewares = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-export default store;
+export const persistor = persistStore(store); 
+
