cbcf15c3e8942f86305b8821a77356c7d59aa42d
samuel <samuellyworld@gmail.com>
2021-09-26 13:44:56 -0700

    collection page fully built and rerouted

--- src/Pages/Collection/CollectionPage.jsx
+++ src/Pages/Collection/CollectionPage.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './CollectionPage.scss';
+import CollectionItem from '../../components/Collection-Item/Collection-Item';
 
 import {connect} from 'react-redux';
 import {selectCollection} from '../../redux/shop/shop-selectors';
 
 
 const CollectionPage = ({collection}) => {
-  console.log(collection);
+  const {title, items} = collection;
 	return (
-		<div className='category'>
-           <h1> category page </h1>
+		<div className='collection-page'>
+           <h2 className='title'>{title}</h2>
+           <div className='items'> 
+            {
+            	items.map(item => 
+              <CollectionItem key={item.id} item={item} /> 
+            		)
+              }
+            </div>
        </div>	
 		)
   
