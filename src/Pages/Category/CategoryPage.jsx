df71aee7468232df2e3ca8042e296191fa01d465
samuel <samuellyworld@gmail.com>
2021-09-25 17:52:30 -0700

    building category page

---
+++ src/Pages/Category/CategoryPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CategoryPage.scss';
+
+
+const CategoryPage = () => (
+  <div className='category'>
+   <h1> category page </h1>
+ </div>
+	)
+
+
+export default CategoryPage;
--- src/Pages/Shop/ShopPage.jsx
+++ src/Pages/Shop/ShopPage.jsx
@@ -1,12 +1,15 @@
  import React from 'react';
 import CollectionOverview from '../../components/Collection-Overview/Collection-Overview';
+import {Route} from 'react-router-dom';
+import CategoryPage from '../Category/CategoryPage';
 
-const ShopPage = ({collections}) => {
+
+const ShopPage = ({match}) => {
 		return (
 			<div className='shop-page'>
-			 <CollectionOverview/>
+			 <Route exact path={`${match.path}`} component={CollectionOverview} />
+             <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
 			</div> 
-
 			)
 	}
 
