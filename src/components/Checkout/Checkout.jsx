0e21940a4b33271a911faee691953a60a5ccb2f9
samuel <samuellyworld@gmail.com>
2021-09-28 18:30:19 -0700

    styled the stripe button

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -34,6 +34,11 @@
       <div className = 'total'>
          <span> TOTAL: ${total}</span>
       </div>
+      <div className='test-warning'>
+           *Please use the following test credit card for payments*
+           <br/>
+           4242 4242 4242 4242 - Exp: 03/21 -CVV : 123
+       </div> 
       <StripeCheckoutButton price={total} /> 
      </div>
      )
