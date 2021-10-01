08075fdcbb0dba66237410e71eb0595d867b2b7b
samuel <samuellyworld@gmail.com>
2021-10-01 13:00:30 -0700

    adding shop data to firestore at one go to be removed later

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import {setCurrentUser} from './redux/user/user-action';
 import {createStructuredSelector} from 'reselect';
 import {selectCurrentUser} from './redux/user/user-selectors';
 
-import {auth, createUserProfileDocument} from './firebase/firebase';
+import {auth, createUserProfileDocument, addCollectionAndDocuments} from './firebase/firebase';
+import {selectCollectionsForPreview} from './redux/shop/shop-selectors';
 
 import './App.scss';
 import CheckoutPage from './Pages/CheckoutPage/CheckoutPage';
@@ -19,7 +20,7 @@ import SignInAndSignUpPage from './Pages/Sign-in-and-sign-up/Sign-in-and-sign-up
 class App extends React.Component {
 unsubscribeFromAuth = null;
 componentDidMount() {
-   const {setCurrentUser} = this.props
+   const {setCurrentUser, collectionArray} = this.props
   this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
     if(userAuth) {
       const userRef = await createUserProfileDocument(userAuth);
@@ -32,6 +33,7 @@ componentDidMount() {
       })
     } 
       setCurrentUser(userAuth);
+      addCollectionAndDocuments('collections', collectionArray)
   
   })
 }
@@ -56,11 +58,13 @@ render() {
 }
 }
 const mapStateToProps = createStructuredSelector({
-  currentUser : selectCurrentUser
+  currentUser : selectCurrentUser,
+  collectionArray : selectCollectionsForPreview
 })
 
 const mapDispatchToProps = dispatch => ({
- setCurrentUser: user => dispatch(setCurrentUser(user))
+ setCurrentUser: user => dispatch(setCurrentUser(user)),
+ 
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -32,6 +32,11 @@ export const createUserProfileDocument = async(userAuth, additionalData) => {
      return userRef;
 }
 
+export const addCollectionAndDocuments =(collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey)
+    console.log(collectionRef)
+}
+
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
