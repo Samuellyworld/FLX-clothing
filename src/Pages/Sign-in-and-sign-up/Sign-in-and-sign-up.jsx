7a9778e9472667552f621829af5c3be561bb3fe1
samuel <samuellyworld@gmail.com>
2021-10-23 08:22:34 -0700

    finished with signin and signup rendering

--- src/Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.jsx
+++ src/Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import SignIn from '../../components/Sign-In/Sign-In';
 import SignUp from '../../components/Sign-Up/Sign-Up';
+import {selectMobileMedia} from '../../redux/media-query/query-selectors';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
 import './Sign-in-and-sign-up.scss';
 
-const SignInAndSignUpPage = () => (
+const SignInAndSignUpPage = ({currentMedia}) => (
      <div className='sign-in-and-sign-up'> 
-     <SignIn /> 
-     <SignUp/>
-     </div>
+          
+             {  currentMedia ?
+                <SignIn /> :
+                (
+                  <div className='sign-in-and-sign-up'>
+                     <SignIn /> 
+                     <SignUp/>
+                 </div>
+               ) 
+            }
+      </div>
 	)
 
-export default SignInAndSignUpPage;
+const mapStateToProps = createStructuredSelector({
+     currentMedia : selectMobileMedia
+})
+export default connect(mapStateToProps)(SignInAndSignUpPage);
