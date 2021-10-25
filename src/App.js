5a0419bdeecd154432f5305f3b90ba5cbe0476b6
samuel <samuellyworld@gmail.com>
2021-10-25 04:48:06 -0700

    fixing and destructuring

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy, Suspense} from 'react';
 
 import { Switch, Route, Redirect} from 'react-router-dom';
 
@@ -12,13 +12,15 @@ import {selectMobileMedia} from './redux/media-query/query-selectors';
 import {auth, createUserProfileDocument} from './firebase/firebase';
 
 import './App.scss';
-import PasswordReset from './components/Password-Reset/Password-Reset';
-import CheckoutPage from './Pages/CheckoutPage/CheckoutPage';
 import Header from './components/Header/Header';
-import HomePage from './Pages/HomePage/HomePage';
-import ShopPage from './Pages/Shop/ShopPage';
-import SignInAndSignUpPage from './Pages/Sign-in-and-sign-up/Sign-in-and-sign-up';
-import SignUp from './components/Sign-Up/Sign-Up';
+
+const HomePage = lazy(() => import('./Pages/HomePage/HomePage'));
+const ShopPage = lazy(() => import('./Pages/Shop/ShopPage'));
+// const Header = lazy(() => import('./components/Header/Header'));
+const CheckoutPage = lazy(() => import('./Pages/CheckoutPage/CheckoutPage'));
+const PasswordReset = lazy(() => import('./components/Password-Reset/Password-Reset'));
+const SignInAndSignUpPage = lazy(() => import('./Pages/Sign-in-and-sign-up/Sign-in-and-sign-up'));
+const SignUp = lazy(() => import('./components/Sign-Up/Sign-Up'));
 
 class App extends React.Component {
 unsubscribeFromAuth = null;
@@ -73,22 +75,23 @@ render() {
   const {currentUser, currentMobileMedia} = this.props
   return (
     <div className="App">
-    <Header />
-     <Switch>
-       <Route exact path='/' component={HomePage} />
-       <Route path='/shop' component={ShopPage} />
-       <Route path='/signin' render={() => currentUser ? (<Redirect to ='/' />): (<SignInAndSignUpPage />) } />
-       <Route exact path='/checkout' component={CheckoutPage} /> 
-       <Route exact path='/reset password' component={PasswordReset} />
-        {
-          currentMobileMedia ? (
-             <Route path='/register' render={() => this.props.currentUser ? (<Redirect to ='/' />): (<SignUp />) } />
-            ) : null
-         
-        }
-        <Route path='*' component={HomePage}/>
-       
-      </Switch>
+      <Header />
+       <Switch>
+         <Suspense fallback={<div> ..loding </div> }>
+           <Route exact path='/' component={HomePage} />
+           <Route path='/shop' component={ShopPage} />
+           <Route path='/signin' render={() => currentUser ? (<Redirect to ='/' />): (<SignInAndSignUpPage />) } />
+           <Route exact path='/checkout' component={CheckoutPage} /> 
+           <Route exact path='/reset password' component={PasswordReset} />
+            {
+              currentMobileMedia ? (
+                 <Route path='/register' render={() => this.props.currentUser ? (<Redirect to ='/' />): (<SignUp />) } />
+                ) : null
+             
+            }
+            <Route path="404" component={HomePage}/>
+          </Suspense>
+        </Switch>
     </div>
   );
 }
